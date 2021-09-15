210131139a9b18decc158d4fbc116cb029cf0527
Mateus Rachid <mateus_rachid@outlook.com>
2021-09-15 18:32:10 -0300

    updated dashboard

--- handle-config.js
+++ handle-config.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const YAML = require('js-yaml');
+var path = require('path')
 
 const config = YAML.load(
   fs.readFileSync('website.config.yaml')
 );
+
+const assetsDir = 'user-defined-assets';
+fs.mkdirSync(`dashboard/public/${assetsDir}`,{recursive:true});
+
+const srcLogoFile = config.dashboard.theme.assets.logo;
+const logoPath = `/${assetsDir}/logo${path.extname(srcLogoFile)}`;
+config.dashboard.theme.assets.logo = logoPath;
+fs.copyFileSync(srcLogoFile,`dashboard/public${logoPath}`);
 
 fs.writeFileSync('src/api/website.config.json',
   JSON.stringify(
@@ -12,7 +21,6 @@ fs.writeFileSync('src/api/website.config.json',
 );
 
 fs.copyFileSync('src/api/website.config.json','dashboard/website.config.json');
-
 
 const firebaserc = {
   projects: {
