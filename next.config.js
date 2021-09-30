3d8fdc41956e521ec8a93c0f1e3987f6cb5af6a1
Mateus Rachid <mateus_rachid@outlook.com>
2021-09-30 18:49:12 -0300

    next rewrites from firebase

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
 const TerserPlugin = require('terser-webpack-plugin');
+
+const firebaseConfig = JSON.parse(
+  fs.readFileSync('./firebase.json')
+);
+
+const rewrites = (firebaseConfig.hosting.rewrites || []).filter(
+  rw => rw.destination && rw.source !== '**'
+).map(rw => ({
+  source: rw.source.replace(/\*\*/g,':path*').replace(/(?<!:path*)\*/g,(_,idx)=>`:argAt${idx}`),
+  destination: rw.destination.replace(/(?<=\/)index.html$/,''),
+}));
 
 module.exports = {
   trailingSlash: true,
+
+  rewrites: async()=>rewrites,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 
