4bc479de55df9983ab573a761b394877443ff861
Mateus Rachid <mateus_rachid@outlook.com>
2021-11-30 14:52:38 -0300

    improved code version embedding

--- scripts/get-code-version.sh
+++ scripts/get-code-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dirty=$([[ $(git status --porcelain) ]] && echo -n dirty)
-dirty=$([[ $dirty ]] && echo -n -$dirty)
-commit=$(git rev-list HEAD^1..HEAD)
+dirty=$([[ $dirty ]] && echo -n -$dirty-$(git diff | sha256sum | grep -Eo '^.{8}'))
+commit=$(git rev-list HEAD^1..HEAD | grep -Eo '^.{8}')
 
 echo -n \"$commit$dirty\"
