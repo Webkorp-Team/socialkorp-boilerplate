83a52f175418c15e789bb47245e6259d60d28f30
Mateus Rachid <mateus_rachid@outlook.com>
2021-11-30 14:49:55 -0300

    add svg support to ImageUpload component

--- src/components/ImageUpload/InputBlobReducer.js
+++ src/components/ImageUpload/InputBlobReducer.js
@@ -10,27 +10,50 @@ reducer._create_blob = function (env) {
     });
 };
 
+const pngReducer = imageBlobReduce();
+pngReducer._create_blob = function (env) {
+  return this.pica.toBlob(env.out_canvas, 'image/png', 0.85)
+    .then(function (blob) {
+      env.out_blob = blob;
+      return env;
+    });
+};
+
 export default function InputBlobReducer({
   onChange=()=>{},
+  accept="image/*",
   ...props
 }){
 
   const handleChange = useCallback((e)=>{
     if(!e.target.files[0])
       return;
-    reducer.toBlob(e.target.files[0],{max:1600}).then(resizedImage => {
+    const noResize = ['image/svg','image/svg+xml'].includes(accept);
+    const pngOnly = (accept === 'image/png');
+    if(noResize){
       const reader = new FileReader();
       reader.addEventListener("load", function () {
         onChange({
           value: reader.result,
-          display: URL.createObjectURL(resizedImage)
+          display: reader.result,
         });
       }, false);
       reader.readAsDataURL(resizedImage);
-    });
+    }else{
+      ( pngOnly ? pngReducer : reducer ).toBlob(e.target.files[0],{max:1600}).then(resizedImage => {
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+          onChange({
+            value: reader.result,
+            display: URL.createObjectURL(resizedImage)
+          });
+        }, false);
+        reader.readAsDataURL(resizedImage);
+      });
+    }
   },[onChange]);
 
   return (
-    <input onChange={handleChange} type="file" accept="image/*" {...props} />
+    <input onChange={handleChange} type="file" accept={accept} {...props} />
   );
 }
