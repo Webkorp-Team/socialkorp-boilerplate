cf63965f61b473bcdba00d9a6a536eaaf0f5a12e
Mateus Rachid <mateus_rachid@outlook.com>
2021-11-30 14:50:23 -0300

    improved ContentEditable component for content parsing

--- src/components/ContentEditable/index.js
+++ src/components/ContentEditable/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { cloneElement, createElement, createContext, useCallback, useContext, useMemo, useState, Fragment, useEffect } from "react";
+import { cloneElement, createElement, createContext, useCallback, useContext, useMemo, useState, Fragment, useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const onUpdateContext = createContext(()=>{});
@@ -16,9 +16,12 @@ function StyledChild({children,className}){
   });
 }
 
-const Text = styled(StyledChild)`
+const Text = styled.span`
+  font: inherit;
+  color: inherit;
   white-space: pre-wrap;
   overflow-wrap: break-word;
+  &[data-editable],
   &[contenteditable]{
     border: 1px dashed transparent;
     &:not(:focus):hover{
@@ -34,10 +37,6 @@ const Text = styled(StyledChild)`
   }
 `;
 
-const Span = styled.span`
-  font: inherit;
-  color: inherit;
-`;
 
 
 
@@ -47,21 +46,23 @@ export default function ContentEditable({
   children: childrenFromProps,
   component: Component,
   readOnly,
-  parserComponent = Fragment,
-  ...propsFromAbove
+  parserComponent: ParserComponent = Fragment,
+  ...props
 }){
 
   const [focus, setFocus] = useState(false);
-  const [counter, setCounter] = useState(0);
+  const [inputInitialized, setInputInitialized] = useState(false);
 
   const onUpdate = useOnEditableElementUpdate();
 
+  const inputRef = useRef();
+
   const handleBlur = useCallback((e)=>{
     setFocus(false);
-    setCounter(x=>x+1);
+    setInputInitialized(false);
     return onUpdate({
       elementName,
-      value: e.target.innerText,
+      value: e.currentTarget.innerText.replace(/\n$/,''),
     });
   },[onUpdate]);
 
@@ -69,24 +70,58 @@ export default function ContentEditable({
     setFocus(true);
   },[]);
 
-  const previewingProps = {
-    contentEditable: true,
-    suppressContentEditableWarning: true,
-    onKeyDown: e=>{
-      if(e.keyCode===27)//esc
-        e.target.blur();
-    },
-    onBlur: handleBlur,
-    onFocus: handleFocus,
-    key: counter,
-    ...propsFromAbove
-  };
+  const [counter, setCounter] = useState(0);
+  useEffect(()=>{
+    if(!focus)
+      return;
+    if(!inputRef.current){
+      setCounter(x=>x+1);
+      return;
+    }
+    if(inputInitialized)
+      return;
+
+    inputRef.current.innerText = section.get(elementName);
+    inputRef.current.focus();
+
+    setInputInitialized(true);
+  },[focus,inputInitialized,section,elementName,counter]);
 
   const router = useRouter();
 
-  const editable = Boolean(elementName);
+  const editable = Boolean(section && elementName);
 
   const previewing = router.pathname === '/preview' && editable;
+
+  const inputProps =  {
+    contentEditable: true,
+    onKeyDown: e=>{
+      if(e.keyCode===27)// esc
+        e.target.blur();
+      if (e.keyCode == 13){ // enter/return
+        // e.stopPropagation();
+        // e.preventDefault();
+        // var selection = window.getSelection();
+        // if(!selection)
+        //   return;
+        // const range = selection.getRangeAt(0);
+        // const br = document.createElement("br");
+        // range.deleteContents();
+        // range.insertNode(br);
+        // range.insertNode(br);
+        // range.collapse();
+      }
+    },
+    onInput: e=>{
+      // e.currentTarget.querySelector('br:last-child')?.remove();
+      // if(!e.currentTarget.innerText.endsWith("\n")){
+      //   const br = document.createElement("br");
+      //   e.currentTarget.appendChild(br);
+      // }
+    },
+    onBlur: handleBlur,
+    onFocus: handleFocus
+  };
 
   if(section && childrenFromProps)
     throw new Error('Either pass a section or a children');
@@ -94,25 +129,21 @@ export default function ContentEditable({
   if(section && !elementName)
     throw new Error("Can't use property section without elementName");
 
-  const props = {
-    ...(previewing ? previewingProps : propsFromAbove)
-  };
-
-  const ParserComponent = previewing && focus ? Fragment : parserComponent;
-
-  const children = (
-    <ParserComponent>
-      {(section ? section.get(elementName) : childrenFromProps) || (
-        previewing ? '<empty>' : null
-      )}
-    </ParserComponent>
+  const children = previewing && focus ? (
+    <Text ref={inputRef} key="edit" {...inputProps}/>
+  ):(
+    <Text data-display={true} data-editable={previewing} onClick={previewing ? handleFocus : undefined} key="display">
+      <ParserComponent key="display">{
+        (section ? section.get(elementName) : childrenFromProps) || (
+          previewing ? '<empty>' : null
+        )
+      }</ParserComponent>
+    </Text>
   );
 
-  const renderingResult = (
+  return (
     Component ? <Component {...props} children={children}/> :
     children && children.type ? cloneElement(children,props) :
-    <Span {...props} children={children}/>
+    <Fragment {...props} children={children}/>
   );
-
-  return <Text>{renderingResult}</Text>;
 }
