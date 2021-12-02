69a79135b940ed9a27d04e04860c4ba42da892db
Mateus Rachid <mateus_rachid@outlook.com>
2021-12-02 14:36:44 -0300

    Link component - out-of-box support for external links

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,12 +1,15 @@
 import NextLink from "next/link";
 
 
-export default function Link({href,displayHref,preview,...props}){
-  const style = preview ? {cursor: 'default'} : undefined;
-  if(href && !preview)
-    return <NextLink href={href} passHref={true} displayHref={displayHref}>
-      <a style={style} {...props}/>
-    </NextLink>;
-  else
-    return <a style={style} {...props}/>;
+export default function Link({href,displayHref,preview,external=false,...props}){
+  if(href && !preview){
+    if(external){
+      return <a href={href} rel="noopener noreferrer" target="_blank" {...props}/>;
+    }else{
+      return <NextLink href={href} passHref={true} displayHref={displayHref}>
+        <a {...props}/>
+      </NextLink>;
+    }
+  }else
+    return <a style={preview ? {cursor: 'default'} : undefined} {...props}/>;
 }
