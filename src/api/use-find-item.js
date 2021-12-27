d1ec63a05ff7d16643625b4998b17938f7530d54
Mateus Rachid <mateus_rachid@outlook.com>
2021-12-27 15:40:52 -0300

    lists - findMany

--- src/api/use-find-item.js
+++ src/api/use-find-item.js
@@ -20,3 +20,23 @@ export default function useFindItem(listName,query){
 
   return item;
 }
+
+export function useFindMany(listName,query){
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    if(!listName || !query)
+      return;
+    Api.get('/list/items',{
+      listName,
+      query,
+    }).then(result => {
+      setItems(result);
+    }).catch((err)=>{
+      console.error(err);
+      setItems(null);
+    });
+  },[listName,JSON.stringify(query)]);
+
+  return items;
+}
