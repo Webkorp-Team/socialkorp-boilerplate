e7e5bcbb82b3b7b868f6ecaed3296eaed2d55ec9
Mateus Rachid <mateus_rachid@outlook.com>
2021-12-27 15:42:31 -0300

    Add scroll option to Link component

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,12 +1,12 @@
 import NextLink from "next/link";
 
 
-export default function Link({href,displayHref,preview,external=false,...props}){
+export default function Link({href,displayHref,preview,external=false,scroll=true,...props}){
   if(href && !preview){
     if(external){
       return <a href={href} rel="noopener noreferrer" target="_blank" {...props}/>;
     }else{
-      return <NextLink href={href} passHref={true} displayHref={displayHref}>
+      return <NextLink href={href} passHref={true} as={displayHref} scroll={scroll}>
         <a {...props}/>
       </NextLink>;
     }
